6f706a1d4950f1e573e688f02620bb3ad07f3f80
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-03-16 20:52:49 +0600

    commit

--- src/components/Map/OpenStreetMap.js
+++ src/components/Map/OpenStreetMap.js
@@ -1,12 +1,23 @@
-import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import React from 'react';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const OpenStreetMap = (props) => {
 	const {location, address} = props;
   return (
-  	<div>
-    <MapContainer center={location} zoom={20} scrollWheelZoom={false} style={{height:"50vh", width:"100%", margin: "auto"}}>
+    <MapContainer center={location} zoom={20} scrollWheelZoom={false} style={{height:"80vh", width:"100%", margin: "auto", borderRadius: 5}}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -17,7 +28,6 @@ const OpenStreetMap = (props) => {
     </Popup>
   </Marker>
 </MapContainer>
-  	</div>
   );
 };
 
